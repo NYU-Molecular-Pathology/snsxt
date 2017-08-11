75fb90a466753a428a0789f0ccbe892d13dbbcab
Stephen Kelly <stevekm@users.noreply.github.com>
2017-08-11 18:05:33 -0400

    refactor package directory

---
+++ test/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+unit tests for the program
+'''
+import pkgutil
+import unittest
+
+
+# def all_names():
+#     for i, modname, j in pkgutil.iter_modules(__path__):
+#         if modname.startswith('test_'):
+#             yield modname
+#
+# def all():
+#     names = [name for name in all_names()]
+#     return unittest.defaultTestLoader.loadTestsFromNames(names)
---
+++ test/test_find.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+unit tests for the program
+'''
+import unittest
+from ..find import multi_filter
+
+class TestMultiFilter(unittest.TestCase):
+
+    def test_fail(self):
+        self.assertTrue(False, 'failure message goes here')
+
+if __name__ == '__main__':
+    unittest.main()
