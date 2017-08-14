a7b88179f78244ec131a5f7954f6c3c0f9995c75
Stephen Kelly <stevekm@users.noreply.github.com>
2017-08-14 14:16:23 -0400

    update find test

--- snsxt/test_find.py
+++ snsxt/test_find.py
@@ -2,15 +2,33 @@
 # -*- coding: utf-8 -*-
 
 '''
-unit tests for the program
+unit tests for the find module
 '''
 import unittest
-from .find import multi_filter
+from find import multi_filter
+from find import super_filter
 
-class TestMultiFilter(unittest.TestCase):
+class TestSuperFilter(unittest.TestCase):
+    def test_true(self):
+        self.assertTrue(True, 'Demo assertion')
 
     def test_fail(self):
-        self.assertTrue(True, 'Demo assertion')
+        self.assertFalse(False)
+
+    def test_error(self):
+        self.assertRaises(ValueError)
+
+    def test_super_filter_all_Eqw(self):
+        filenames = ['fixtures/qstat_stdout_all_Eqw.txt', 'fixtures/qstat_stdout_r_Eqw.txt', 'fixtures/qstat_stdout_Eqw_qw.txt']
+        match_result = [x for x in super_filter(names = filenames, inclusion_patterns = "*Eqw*")]
+        self.assertTrue(filenames == match_result)
+
+    def test_super_filter_all_Eqw_fail(self):
+        filenames = ['fixtures/qstat_stdout_all_Eqw.txt', 'fixtures/qstat_stdout_r_Eqw.txt', 'fixtures/qstat_stdout_Eqw_qw.txt']
+        match_result = [x for x in super_filter(names = filenames, inclusion_patterns = "*E1qw*")]
+        self.assertFalse(filenames == match_result)
+
+
 
 if __name__ == '__main__':
     unittest.main()
