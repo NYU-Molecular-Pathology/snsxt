103b9f85aa1a23dc7dd339b17b6baa1e5e9574b2
Stephen Kelly <stevekm@users.noreply.github.com>
2017-08-21 12:30:31 -0400

    update test.py

--- snsxt/test.py
+++ snsxt/test.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     loader = unittest.TestLoader()
     start_dir = '.'
     suite = loader.discover(start_dir)
-    # print('suite is: {0}'.format(suite))
 
     runner = unittest.TextTestRunner()
     runner.run(suite)
