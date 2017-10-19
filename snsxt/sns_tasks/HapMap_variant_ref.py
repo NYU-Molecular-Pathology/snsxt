ed98dba1447d0db1eb5d3f861adfe0c3bbc01e07
Stephen Kelly <stevekm@users.noreply.github.com>
2017-10-19 16:16:37 -0400

    add JSON output to hapmap module

--- snsxt/sns_tasks/HapMap_variant_ref.py
+++ snsxt/sns_tasks/HapMap_variant_ref.py
@@ -63,8 +63,7 @@ configs['input_dir'] = config.HapMap_variant_ref['input_dir']
 configs['input_pattern'] = config.HapMap_variant_ref['input_pattern']
 configs['output_dir_name'] = config.HapMap_variant_ref['output_dir_name']
 configs['hapmap_variant_file'] = config.HapMap_variant_ref['hapmap_variant_file']
-
-
+configs['hapmap_sample_stats_file'] = config.HapMap_variant_ref['hapmap_sample_stats_file']
 
 configs['report_files'] = config.HapMap_variant_ref['report_files']
 configs['task_files'] = config.HapMap_variant_ref['task_files']
@@ -94,24 +93,43 @@ def main(sample, extra_handlers = None):
     logger.debug('output_dir: {0}'.format(output_dir))
 
     # ~~~~~ TASK SPECIFIC CUSTOM ITEMS ~~~~~ #
+    import re
+    import json
+
     logger.debug('Sample is: {0}'.format(sample))
 
-    # file with the sample's ANNOVAR annotations
-    sample_annot_file = sample.list_none(sample.get_output_files(analysis_step = configs['input_dir'], pattern = configs['input_pattern']))
-    logger.debug('sample_annot_file is: {0}\nand has {1} entries'.format(sample_annot_file, t.num_lines(sample_annot_file, skip = 1)))
+    # dict to hold info about the hapmap samples and their files
+    hapmap_sample_stats = {}
+    hapmap_sample_stats_filepath = os.path.join(output_dir, configs['hapmap_sample_stats_file'])
 
-    # reference HapMap variants file; copy it over
-    hapmap_variant_file = task_utils.setup_task_file(configs['hapmap_variant_file'], output_dir, configs)
-    logger.debug('hapmap_variant_file is: {0}\nand has {1} entries'.format(hapmap_variant_file, t.num_lines(hapmap_variant_file, skip = 1)))
+    # check if the sample ID indicates that it is HapMap sample
+    if re.match('hapmap', sample.id, re.IGNORECASE):
+        # file with the sample's ANNOVAR annotations
+        sample_annot_file = sample.list_none(sample.get_output_files(analysis_step = configs['input_dir'], pattern = configs['input_pattern']))
+        logger.debug('sample_annot_file is: {0}\nand has {1} entries'.format(sample_annot_file, t.num_lines(sample_annot_file, skip = 1)))
 
-    # file to save the overlaps to
-    output_file = os.path.join(output_dir, os.path.basename(sample_annot_file))
-    logger.debug('Output file will be: {0}'.format(output_file))
+        # reference HapMap variants file; copy it over
+        hapmap_variant_file = task_utils.setup_task_file(configs['hapmap_variant_file'], output_dir, configs)
+        logger.debug('hapmap_variant_file is: {0}\nand has {1} entries'.format(hapmap_variant_file, t.num_lines(hapmap_variant_file, skip = 1)))
 
-    # make the overlaps
-    logger.debug('Overlapping the sample_annot_file against the hapmap_variant_file...')
-    t.write_tabular_overlap(file1 = sample_annot_file, ref_file = hapmap_variant_file, output_file = output_file, delim = '\t')
-    logger.debug('{0} overlaps were output'.format(t.num_lines(output_file, skip = 1)))
+        # file to save the overlaps to
+        output_file = os.path.join(output_dir, os.path.basename(sample_annot_file))
+        logger.debug('Output file will be: {0}'.format(output_file))
+
+        # make the overlaps
+        logger.debug('Overlapping the sample_annot_file against the hapmap_variant_file...')
+        t.write_tabular_overlap(file1 = sample_annot_file, ref_file = hapmap_variant_file, output_file = output_file, delim = '\t')
+        logger.debug('{0} overlaps were output'.format(t.num_lines(output_file, skip = 1)))
+
+        # save a JSON with info about the sample ID's and their files
+        hapmap_sample_stats[sample.id] = {'file': output_file}
+
+        logger.debug('hapmap_sample_stats_filepath: {0}'.format(hapmap_sample_stats_filepath))
+        t.update_json(data = hapmap_sample_stats, input_file = hapmap_sample_stats_filepath)
+
+    else:
+        logger.debug('Sample is not a HapMap sample, skipping.')
+
 
 
     # set up the report
