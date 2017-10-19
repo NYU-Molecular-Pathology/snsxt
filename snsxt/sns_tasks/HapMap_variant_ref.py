3e32ce124f2e7baa729681adb08490482e124525
Stephen Kelly <stevekm@users.noreply.github.com>
2017-10-19 15:32:29 -0400

    add overlapping to Hapmap module

--- snsxt/sns_tasks/HapMap_variant_ref.py
+++ snsxt/sns_tasks/HapMap_variant_ref.py
@@ -73,104 +73,6 @@ configs['task_files'] = config.HapMap_variant_ref['task_files']
 
 
 # ~~~~ CUSTOM FUNCTIONS ~~~~~~ #
-# def make_tresholds_arg():
-#     '''
-#     Make the command line arg for the thresholds to use
-#     ex: -ct 10 -ct 50 -ct 100 -ct 500
-#
-#     d = {'thresholds': [10, 50, 100, 200, 500]}
-#     '''
-#     thresholds = configs['thresholds']
-#     return(' '.join(['-ct ' + str(x) for x in thresholds]))
-
-# def gatk_DepthOfCoverage_cmd(sampleID, bam_file, intervals_bed_file, output_dir):
-#     '''
-#     Build the terminal commands to run GATK DepthOfCoverage on a single sample
-#
-#     ex:
-#     $gatk_cmd -T DepthOfCoverage -dt NONE $gatk_log_level_arg \
-#     -rf BadCigar \
-#     --reference_sequence $ref_fasta \
-#     --intervals $bed \
-#     --omitDepthOutputAtEachBase \
-#     -ct 10 -ct 50 -ct 100 -ct 500 -mbq 20 -mmq 20 --nBins 999 --start 1 --stop 1000 \
-#     --input_file $bam \
-#     --outputFormat csv \
-#     --out $out_prefix
-#     '''
-#     # get params from config
-#     GATK_bin = configs['bin']
-#     ref_fasta = configs['ref_fasta']
-#     minBaseQuality = configs['minBaseQuality']
-#     minMappingQuality = configs['minMappingQuality']
-#     nBins = configs['nBins']
-#     start = configs['start']
-#     stop = configs['stop']
-#     outputFormat = configs['outputFormat']
-#     readFilter = configs['readFilter']
-#     downsampling_type = configs['downsampling_type']
-#     thresholds_arg = make_tresholds_arg()
-#     output_summary_file = os.path.join(output_dir, '{0}'.format(sampleID))
-#
-#     gatk_cmd = '''
-# java -Xms16G -Xmx16G -jar {0} -T DepthOfCoverage \
-# --logging_level ERROR \
-# --downsampling_type {1} \
-# --read_filter {2} \
-# --reference_sequence {3} \
-# --omitDepthOutputAtEachBase \
-# {4} \
-# --intervals {5} \
-# --minBaseQuality {6} \
-# --minMappingQuality {7} \
-# --nBins {8} \
-# --start {9} \
-# --stop {10} \
-# --input_file {11} \
-# --outputFormat {12} \
-# --out {13}
-# '''.format(
-# GATK_bin,
-# downsampling_type,
-# readFilter,
-# ref_fasta,
-# thresholds_arg,
-# intervals_bed_file,
-# minBaseQuality,
-# minMappingQuality,
-# nBins,
-# start,
-# stop,
-# bam_file,
-# outputFormat,
-# output_summary_file
-# )
-#     return(gatk_cmd)
-
-def get_report_files():
-    '''
-    Get the files for the report based on the configs, return a list
-    '''
-    report_files = []
-    report_dir = os.path.join(scriptdir, configs['report_dir'])
-    for item in configs['report_files']:
-        file_path = os.path.join(report_dir, item)
-        report_files.append(file_path)
-    return(report_files)
-
-def setup_report(output_dir):
-    '''
-    Set up the report files output for the pipeline step
-    by copying over every associated file for the report to the output dir
-    '''
-    report_files = get_report_files()
-    logger.debug("Report files are: {0}".format(report_files))
-    for item in report_files:
-        output_file = os.path.join(output_dir, os.path.basename(item))
-        logger.debug("Copying report file '{0}' to '{1}' ".format(item, output_file))
-        shutil.copy2(item, output_file)
-
-
 def main(sample, extra_handlers = None):
     '''
     Main control function for the program
@@ -194,35 +96,25 @@ def main(sample, extra_handlers = None):
     # ~~~~~ TASK SPECIFIC CUSTOM ITEMS ~~~~~ #
     logger.debug('Sample is: {0}'.format(sample))
 
-
+    # file with the sample's ANNOVAR annotations
     sample_annot_file = sample.list_none(sample.get_output_files(analysis_step = configs['input_dir'], pattern = configs['input_pattern']))
+    logger.debug('sample_annot_file is: {0}\nand has {1} entries'.format(sample_annot_file, t.num_lines(sample_annot_file, skip = 1)))
 
+    # reference HapMap variants file; copy it over
     hapmap_variant_file = task_utils.setup_task_file(configs['hapmap_variant_file'], output_dir, configs)
+    logger.debug('hapmap_variant_file is: {0}\nand has {1} entries'.format(hapmap_variant_file, t.num_lines(hapmap_variant_file, skip = 1)))
+
+    # file to save the overlaps to
+    output_file = os.path.join(output_dir, os.path.basename(sample_annot_file))
+    logger.debug('Output file will be: {0}'.format(output_file))
+
+    # make the overlaps
+    logger.debug('Overlapping the sample_annot_file against the hapmap_variant_file...')
+    t.write_tabular_overlap(file1 = sample_annot_file, ref_file = hapmap_variant_file, output_file = output_file, delim = '\t')
+    logger.debug('{0} overlaps were output'.format(t.num_lines(output_file, skip = 1)))
 
 
-    # sample_bam = sample.list_none(sample.get_output_files(analysis_step = configs['input_dir'], pattern = configs['input_pattern']))
-    # targets_bed = sample.list_none(sample.get_files('targets_bed'))
-
-    # logger.debug(make_tresholds_arg())
-
-    # if sample_bam and output_dir and qsub_log_dir:
-    #     logger.debug('sample_bam: {0}'.format(sample_bam))
-    #
-    #     # make the shell command to run
-    #     command = gatk_DepthOfCoverage_cmd(sampleID = sample.id, bam_file = sample_bam, output_dir = output_dir, intervals_bed_file = targets_bed)
-    #     logger.debug(command)
-    #
-    #     # submit the command as a qsub job on the HPC
-    #     # commands to create debug jobs
-    #     # command = 'sleep 60'
-    #     # qsub_log_dir = qsub_log_dir[:-1]
-    #     job = qsub.submit(command = command, name = task_name + '.' + sample.id, stdout_log_dir = qsub_log_dir, stderr_log_dir = qsub_log_dir, verbose = True, sleeps = 1) #
-    #     return(job)
-    # else:
-    #     logger.error('A required item does not exist')
-    #
-    #
-    # # set up the report
+    # set up the report
     # task_utils.setup_report(output_dir = output_dir, configs = configs)
 
 def run():
