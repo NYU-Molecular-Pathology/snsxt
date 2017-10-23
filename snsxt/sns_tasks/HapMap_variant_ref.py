b40425048e7b9101a26a26dcadd87c54028aea92
Stephen Kelly <stevekm@users.noreply.github.com>
2017-10-23 11:22:20 -0400

    add HapMap_variant_ref module

--- snsxt/sns_tasks/HapMap_variant_ref.py
+++ snsxt/sns_tasks/HapMap_variant_ref.py
@@ -54,6 +54,8 @@ configs['this_scriptdir'] = scriptdir # /ifs/data/molecpathlab/scripts/snsxt/sns
 configs['this_scriptname'] = scriptname # Summary_Avg_Coverage.py
 configs['logdir'] = logdir # /ifs/data/molecpathlab/scripts/snsxt/snsxt/sns_tasks/logs
 configs['log_file'] = log_file
+configs['task_name'] = task_name
+
 
 # analysis input/output locations
 configs['report_dir'] = config.sns_tasks['report_dir']
@@ -67,7 +69,7 @@ configs['hapmap_sample_stats_file'] = config.HapMap_variant_ref['hapmap_sample_s
 
 configs['report_files'] = config.HapMap_variant_ref['report_files']
 configs['task_files'] = config.HapMap_variant_ref['task_files']
-
+configs['config_file'] = config.HapMap_variant_ref_file
 
 
 
@@ -77,7 +79,7 @@ def main(sample, extra_handlers = None):
     Main control function for the program
     Compare a single HapMap sample variant annotation file
     against a known reference list of HapMap variants
-    output a file over the overlapping variant annotations
+    output a file containing the variant annotations found which were not in the reference variant list
     for use in the report later
     '''
     # ~~~~~ REQUIRED TASK ITEMS ~~~~~ #
@@ -98,42 +100,86 @@ def main(sample, extra_handlers = None):
 
     logger.debug('Sample is: {0}'.format(sample))
 
-    # dict to hold info about the hapmap samples and their files
-    hapmap_sample_stats = {}
-    hapmap_sample_stats_filepath = os.path.join(output_dir, configs['hapmap_sample_stats_file'])
+    # get paths to files for the sample
+    # file with the sample's ANNOVAR annotations
+    sample_annot_file = sample.list_none(sample.get_output_files(analysis_step = configs['input_dir'], pattern = configs['input_pattern']))
+    logger.debug('sample_annot_file is: {0}\nand has {1} entries'.format(sample_annot_file, t.num_lines(sample_annot_file, skip = 1)))
+
+    # reference HapMap variants file
+    # hapmap_variant_file = task_utils.setup_task_file(configs['hapmap_variant_file'], output_dir, configs)
+    hapmap_variant_file = os.path.join(configs['this_scriptdir'], configs['files_dir'], configs['hapmap_variant_file'])
+
+    # JSON file to hold info about the hapmap samples and their files
+    # hapmap_stats_filepath = os.path.join(output_dir, configs['hapmap_sample_stats_file'])
+
+    # name of the reference file
+    # ref_name = os.path.splitext(os.path.basename(hapmap_variant_file))[0]
+
+    # file to save the variants not in the reference list
+    output_file = os.path.join(output_dir, os.path.basename(sample_annot_file))
+
+    # # data to save in the JSON output
+    # data = {}
 
     # check if the sample ID indicates that it is HapMap sample
     if re.match('hapmap', sample.id, re.IGNORECASE):
-        # file with the sample's ANNOVAR annotations
-        sample_annot_file = sample.list_none(sample.get_output_files(analysis_step = configs['input_dir'], pattern = configs['input_pattern']))
-        logger.debug('sample_annot_file is: {0}\nand has {1} entries'.format(sample_annot_file, t.num_lines(sample_annot_file, skip = 1)))
+        logger.debug('Sample is a HapMap sample')
+
+
 
         # reference HapMap variants file; copy it over
-        hapmap_variant_file = task_utils.setup_task_file(configs['hapmap_variant_file'], output_dir, configs)
         logger.debug('hapmap_variant_file is: {0}\nand has {1} entries'.format(hapmap_variant_file, t.num_lines(hapmap_variant_file, skip = 1)))
 
-        # file to save the overlaps to
-        output_file = os.path.join(output_dir, os.path.basename(sample_annot_file))
         logger.debug('Output file will be: {0}'.format(output_file))
 
-        # make the overlaps
+        # find the new variants
         logger.debug('Overlapping the sample_annot_file against the hapmap_variant_file...')
-        t.write_tabular_overlap(file1 = sample_annot_file, ref_file = hapmap_variant_file, output_file = output_file, delim = '\t')
-        logger.debug('{0} overlaps were output'.format(t.num_lines(output_file, skip = 1)))
+        t.write_tabular_overlap(file1 = sample_annot_file, ref_file = hapmap_variant_file, output_file = output_file, delim = '\t', inverse = True)
+        logger.debug('{0} non-overlapping variants were output'.format(t.num_lines(output_file, skip = 1)))
 
         # save a JSON with info about the sample ID's and their files
-        hapmap_sample_stats[sample.id] = {'file': output_file}
+        # logger.debug('hapmap_stats_filepath: {0}'.format(hapmap_stats_filepath))
 
-        logger.debug('hapmap_sample_stats_filepath: {0}'.format(hapmap_sample_stats_filepath))
-        t.update_json(data = hapmap_sample_stats, input_file = hapmap_sample_stats_filepath)
+        # check if the output JSON file already exists
+        # if t.item_exists(hapmap_stats_filepath):
+            # logger.debug('File {0} already exists, loading data'.format(hapmap_stats_filepath))
+            # # import the old data
+            # data = t.load_json(hapmap_stats_filepath)
+            #
+            # # update the 'samples' dict with this sample
+            # try:
+            #     data['samples'].update({sample.id: {'file': output_file} })
+            #     logger.debug('Upated "samples" entry in the data')
+            # except KeyError:
+            #     # or make the entry instead
+            #     logger.debug('"samples" entry not found in the data, it will be created')
+            #     data['samples'] = {sample.id: {'file': output_file} }
+            #
+            # # update the 'ref' dict
+            # try:
+            #     data['ref'].update({ref_name: hapmap_variant_file})
+            #     logger.debug('Upated "ref_name" entry in the data')
+            # except KeyError:
+            #     # or make the entry instead
+            #     logger.debug('"ref_name" entry not found in the data, it will be created')
+            #     data['ref'] = {ref_name: hapmap_variant_file}
+            #
+            # # save the data to the JSON file
+            # logger.debug('Saving the data to file: {0}'.format(hapmap_stats_filepath))
+            # t.write_json(object = data, output_file = hapmap_stats_filepath)
+        # else:
+            # if the file doesn't exist, create the data and save it to the file
+            # logger.debug('File {0} does not exists, data will be created and saved to it'.format(hapmap_stats_filepath))
+            #
+            # data['samples'] = {sample.id: {'file': output_file} }
+            # data['ref'] = {ref_name: hapmap_variant_file}
+            # t.write_json(object = data, output_file = hapmap_stats_filepath)
 
     else:
         logger.debug('Sample is not a HapMap sample, skipping.')
 
-
-
     # set up the report
-    # task_utils.setup_report(output_dir = output_dir, configs = configs)
+    task_utils.setup_report(output_dir = output_dir, configs = configs)
 
 def run():
     '''
