6fed92817f5c76cf0cc435a2a874b2025a860257
Stephen Kelly <stevekm@users.noreply.github.com>
2017-11-22 16:12:44 -0500

    update submodule

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # default settings for bash and Python scripts
+# for attributes that are external to the program
 
 # ~~~~~ Deployment Settings ~~~~~ #
 # settings used for setting up a new clinical NGS580 analysis
