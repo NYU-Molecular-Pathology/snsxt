1502efce74b36413887f9e712332d2bdec5b9fc3
Stephen Kelly <stevekm@users.noreply.github.com>
2017-11-22 14:43:30 -0500

    add default settings files

---
+++ settings.py
@@ -0,0 +1,6 @@
+# default settings for bash and Python scripts
+
+# ~~~~~ Deployment Settings ~~~~~ #
+# settings used for setting up a new clinical NGS580 analysis
+# directory to look for output from the NextSeq
+sequencer_directory="/ifs/data/molecpathlab/quicksilver"
